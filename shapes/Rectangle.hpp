f8aad19b6feaac8b0859523317779677620c4483
adrianow795 <adrianow795@gmail.com>
2025-05-08 08:32:00 +0200

    Override/final

--- shapes/Rectangle.hpp
+++ shapes/Rectangle.hpp
@@ -9,11 +9,11 @@ public:
     Rectangle(double x, double y);
     Rectangle(const Rectangle & other) = default;
 
-    double getArea() const;
-    double getPerimeter() const;
+    double getArea() const override;
+    double getPerimeter() const override;
     double getX() const;
     double getY() const;
-    void print() const;
+    void print() const override;
 
 private:
     
